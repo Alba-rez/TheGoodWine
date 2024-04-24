0d4f07a7ef6c2af08177759e1658869eaf284e0f
Alba-rez <a22albaaa@iessanclemente.net>
2024-04-24 18:59:21 +0200

    AndroidStudio

--- TheGoodWine/app/src/main/java/com/example/thegoodwine/MainActivity.kt
+++ TheGoodWine/app/src/main/java/com/example/thegoodwine/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +24,19 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         val navController=navHostFragment.navController
 
+        // referencia al drawer layout
+        val drawer=findViewById<DrawerLayout>(R.id.drawer_layout)
+
         //configuración de la toolbar
         val builder= AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)
         val appBarConfig=builder.build()
         toolbar.setupWithNavController(navController,appBarConfig)
+
+        // enlazar el drawer con el con el sistema de navegación
+        val navView=findViewById<NavigationView>(R.id.nav_view)
+        navView.setupWithNavController(navController)
+
 
     }
 
