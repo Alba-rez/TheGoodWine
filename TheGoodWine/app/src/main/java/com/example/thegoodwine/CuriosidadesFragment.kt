cdfbb18c2f0d02630c69b5f2b957f690e6bf93c2
Alba-rez <a22albaaa@iessanclemente.net>
2024-04-30 20:17:08 +0200

    AndroidSturdio

---
+++ TheGoodWine/app/src/main/java/com/example/thegoodwine/CuriosidadesFragment.kt
@@ -0,0 +1,32 @@
+package com.example.thegoodwine
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CuriosidadesFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CuriosidadesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_curiosidades, container, false)
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+
+        return view
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        (activity as AppCompatActivity).supportActionBar?.title ="The Good Wine "
+    }
+}
--- TheGoodWine/app/src/main/java/com/example/thegoodwine/SiFragment.kt
+++ TheGoodWine/app/src/main/java/com/example/thegoodwine/SiFragment.kt
@@ -1,5 +1,6 @@
 package com.example.thegoodwine
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,6 +21,7 @@ import com.google.android.material.appbar.MaterialToolbar
 class SiFragment : Fragment() {
 
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,11 +30,14 @@ class SiFragment : Fragment() {
         val view=inflater.inflate(R.layout.fragment_si, container, false)
 
         val ButtonStart=view.findViewById<Button>(R.id.start)
+        val ButtonKnow=view.findViewById<Button>(R.id.know)
 
         ButtonStart.setOnClickListener {
             view.findNavController().navigate(R.id.action_siFragment_to_chooseFragment)
         }
-
+        ButtonKnow.setOnClickListener {
+            view.findNavController().navigate(R.id.action_siFragment_to_curiosidadesFragment)
+        }
         return view
     }
 
